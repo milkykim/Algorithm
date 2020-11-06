0e90682c3e1dd1add68c9943eb17bc6a771dce32
milkyKim <kimminji122258@gmail.com>
2020-11-06 13:20:03 +0900

    bfs/dfs

--- bfs_dfs/targetNumber.py
+++ bfs_dfs/targetNumber.py
@@ -1,18 +1,16 @@
 def solution(numbers, target):
-    # result는 0부터 시작. 그리고, [0, 0-a, 0+a, 0-a-b, 0-a+b, 0+a-b, 0+a+b, ... ] 이런 식으로 간다.
+    # result는 0부터 시작. 하나식 원소를 빼서 더하고 뺀 값이 계속 추가 될 것.
     result = [0]
 
     # i는 numbers의 각 원소들
     for i in range(len(numbers)):
         temp = []
 
-        # result는 0부터 numbers의 각 원소들을 빼고 더한 값들이 있다.
+        
         # result의 값을 하나씩 빼서 number의 한 원소를 더하고 빼서 그 값을 result로 대치해준다.
         for j in range(len(result)):
-            
             temp.append(result[j] - numbers[i])
             temp.append(result[j] + numbers[i])
-            print("temp=>",temp)
 
         result = temp
         print(result)
