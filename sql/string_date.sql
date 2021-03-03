57355c41d669347f5647d1bfa8ba2021da7e4223
milkyKim <kimminji122258@gmail.com>
2021-03-04 00:52:52 +0900

    03/04 SQL(STRING_DATE)

--- sql/string_date.sql
+++ sql/string_date.sql
@@ -7,17 +7,21 @@ ORDER BY ANIMAL_ID
 
 -- 이름에 'EL'이 들어가는 '개' 조회
 -- https://programmers.co.kr/learn/courses/30/lessons/59047?language=mysql
+-- %el : el로 끝나는 것 조회
+-- el% : el로 시작하는 것 조회
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
-WHERE NAME LIKE '%EL%' AND ANIMAL_TYPE='Dog'
+WHERE NAME LIKE '%EL%' AND ANIMAL_TYPE = 'Dog'
 ORDER BY NAME
 
 -- 중성화한 동물 O,X로 표기하기
 -- https://programmers.co.kr/learn/courses/30/lessons/59409?language=mysql
--- REGEXP 함수 -> LIKE IN 합친 함수. 여러개의 문자열이 포함되어있는 행을 꺼내옴
 SELECT ANIMAL_ID, NAME, 
-CASE WHEN (SEX_UPON_INTAKE REGEXP 'Neutered|Spayed') THEN 'O'
-ELSE 'X' END AS "중성화"
+CASE
+    WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O'
+    WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+    ELSE 'X'
+END AS '중성화'
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
 
@@ -28,7 +32,9 @@ FROM ANIMAL_INS INS JOIN ANIMAL_OUTS OUTS
 ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
 ORDER BY OUTS.DATETIME - INS.DATETIME DESC LIMIT 2
 
+
 -- DATETIME에서 DATE로 형 변환
 -- https://programmers.co.kr/learn/courses/30/lessons/59414
 SELECT ANIMAL_ID, NAME, date_format(DATETIME, '%Y-%m-%d') AS "날짜"
 FROM ANIMAL_INS
+
